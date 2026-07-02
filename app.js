f91c752f1a13c15e60cdd30af20e70258f8fc13a
niyjie <honryjoe@gmail.com>
2026-07-02 14:18:58 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("基金驾驶舱V2启动成功");
+const SUPABASE_URL = "https://cetpayxtoydyitahhdqh.supabase.co";
+const SUPABASE_KEY = "sb_publishable_jIBr6C0-fb6p4x1Ng6gfZw_uDCrsJ1a";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
+
+async function loadInvestments() {
+    const { data, error } = await supabase
+        .from("investments")
+        .select("*");
+
+    if (error) {
+        console.error(error);
+        return;
+    }
+
+    console.log("投资记录", data);
+}
