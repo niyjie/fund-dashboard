0c0a458e37fd408d92cc5905e2532f1d7394b097
niyjie <honryjoe@gmail.com>
2026-07-02 14:29:11 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
 const SUPABASE_URL = "https://cetpayxtoydyitahhdqh.supabase.co";
-const SUPABASE_KEY = "sb_publishable_jIBr6C0-fb6p4x1Ng6gfZw_uDCrsJ1a";
 
-const supabase = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
+const SUPABASE_KEY =
+"sb_publishable_jIBr6C0-fb6p4x1Ng6gfZw_uDCrsJ1a";
+
+const sbClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
 );
 
-async function loadInvestments() {
-    const { data, error } = await supabase
-        .from("investments")
-        .select("*");
+window.loadInvestments = async function () {
 
-    if (error) {
-        console.error(error);
-        return;
-    }
+  console.clear();
 
-    console.log("投资记录", data);
-}
+  console.log("开始连接数据库...");
+
+  const { data, error } = await sbClient
+    .from("investments")
+    .select("*");
+
+  if (error) {
+    console.error("数据库错误：", error);
+    return;
+  }
+
+  console.log("读取成功：");
+
+  console.table(data);
+
+  alert(
+    "数据库连接成功\n\n记录数：" +
+    data.length
+  );
+};
